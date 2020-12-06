991d871f9279351cbbd1a9ae1dffe69337b361b7
Lawrence Moronkeji <lawrence-keji@users.noreply.github.com>
2020-12-06 12:00:01 +0100

    Creating a BookForm Component

---
+++ src/components/BookForm.vue
@@ -0,0 +1,24 @@
+<template>
+<form v-on:submit.prevent class="" action="#" method="post">
+    <input type="text" name="title" value="" placeholder="Book Title">
+    <input type="text" name="author" value="" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+</form>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data: {
+        bookTitle: '',
+        bookAuthor: ''
+    }
+}
+</script>
+
+<style scoped>
+li{
+    display: block;
+    margin: 0 10px;}
+</style>
